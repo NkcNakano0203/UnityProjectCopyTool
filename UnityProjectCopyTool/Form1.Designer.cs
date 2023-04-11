fe509afb09111a73df89f981135cdd35d6a41e94
DESKTOP-NS\NShome <sounaka0203@ezweb.ne.jp>
2023-04-11 21:10:45 +0900

    プロジェクト ファイルを追加します。

---
+++ UnityProjectCopyTool/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace UnityProjectCopyTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IncludePackagesCheckBox = new System.Windows.Forms.CheckBox();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.AlwaysOnTopCheckBox = new System.Windows.Forms.CheckBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.OpenFolderDialogButton = new System.Windows.Forms.Button();
+            this.SelectFolderAddressTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // IncludePackagesCheckBox
+            // 
+            this.IncludePackagesCheckBox.AutoSize = true;
+            this.IncludePackagesCheckBox.Location = new System.Drawing.Point(14, 138);
+            this.IncludePackagesCheckBox.Name = "IncludePackagesCheckBox";
+            this.IncludePackagesCheckBox.Size = new System.Drawing.Size(113, 16);
+            this.IncludePackagesCheckBox.TabIndex = 0;
+            this.IncludePackagesCheckBox.Text = "Packagesを含める";
+            this.IncludePackagesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(331, 151);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(107, 32);
+            this.CopyButton.TabIndex = 1;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // AlwaysOnTopCheckBox
+            // 
+            this.AlwaysOnTopCheckBox.AutoSize = true;
+            this.AlwaysOnTopCheckBox.Location = new System.Drawing.Point(14, 160);
+            this.AlwaysOnTopCheckBox.Name = "AlwaysOnTopCheckBox";
+            this.AlwaysOnTopCheckBox.Size = new System.Drawing.Size(157, 16);
+            this.AlwaysOnTopCheckBox.TabIndex = 2;
+            this.AlwaysOnTopCheckBox.Text = "ウィンドウを常に最前面表示";
+            this.AlwaysOnTopCheckBox.UseVisualStyleBackColor = true;
+            this.AlwaysOnTopCheckBox.CheckedChanged += new System.EventHandler(this.AlwaysOnTopCheckBox_CheckedChanged);
+            this.AlwaysOnTopCheckBox.Click += new System.EventHandler(this.AlwaysOnTopCheckBox_CheckedChanged);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // OpenFolderDialogButton
+            // 
+            this.OpenFolderDialogButton.Location = new System.Drawing.Point(331, 64);
+            this.OpenFolderDialogButton.Name = "OpenFolderDialogButton";
+            this.OpenFolderDialogButton.Size = new System.Drawing.Size(31, 19);
+            this.OpenFolderDialogButton.TabIndex = 3;
+            this.OpenFolderDialogButton.Text = "・・・";
+            this.OpenFolderDialogButton.UseVisualStyleBackColor = true;
+            this.OpenFolderDialogButton.Click += new System.EventHandler(this.OpenFolderDialogButton_Click);
+            // 
+            // SelectFolderAddressTextBox
+            // 
+            this.SelectFolderAddressTextBox.Location = new System.Drawing.Point(119, 64);
+            this.SelectFolderAddressTextBox.Name = "SelectFolderAddressTextBox";
+            this.SelectFolderAddressTextBox.ReadOnly = true;
+            this.SelectFolderAddressTextBox.Size = new System.Drawing.Size(206, 19);
+            this.SelectFolderAddressTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 195);
+            this.Controls.Add(this.SelectFolderAddressTextBox);
+            this.Controls.Add(this.OpenFolderDialogButton);
+            this.Controls.Add(this.AlwaysOnTopCheckBox);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.IncludePackagesCheckBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox IncludePackagesCheckBox;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.CheckBox AlwaysOnTopCheckBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button OpenFolderDialogButton;
+        private System.Windows.Forms.TextBox SelectFolderAddressTextBox;
+    }
+}
+
---
+++ UnityProjectCopyTool/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace UnityProjectCopyTool
+{
+    public partial class Form1 : Form
+    {
+        readonly string[] copyFolderNames =
+        {
+            "Assets","Library","ProjectSettings"
+        };
+        const string packagesStr = "Packages";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFolderDialogButton_Click(object sender, EventArgs e)
+        {
+            // フォルダを選択するためのダイアログを表示する
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            // 選択されたフォルダのパスを取得する
+            if (result != DialogResult.OK || string.IsNullOrWhiteSpace(dialog.SelectedPath)) return;
+            string selectedPath = dialog.SelectedPath;
+
+            // 選択されたフォルダのパスを表示する
+            SelectFolderAddressTextBox.Text = selectedPath;
+        }
+
+        private void AlwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // チェックが入っていたら最前面表示する
+            TopMost = AlwaysOnTopCheckBox.Checked;
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            // Packagesの確認
+            bool includePackages = IncludePackagesCheckBox.Checked;
+
+            string folderPass = SelectFolderAddressTextBox.Text;
+            // フォルダ名の取得
+            string folderName = Path.GetFileName(folderPass);
+            // 子フォルダのパスを取得
+            string[] subFoldersPass = Directory.GetDirectories(folderPass);
+
+            // 選択されたフォルダの親フォルダのパスを取得する
+            string parentFolderPath = Directory.GetParent(folderPass).FullName;
+
+            // 新しいフォルダを作成する
+            string newFolderName = $"{folderName}_Copy";
+            string newFolderPath = Path.Combine(parentFolderPath, newFolderName);
+            Directory.CreateDirectory(newFolderPath);
+
+            foreach (string subFolderPass in subFoldersPass)
+            {
+                string subFolderName = Path.GetFileName(subFolderPass);
+                foreach (var item in copyFolderNames)
+                {
+                    // フォルダ名で識別してコピーする
+                    if (subFolderName != item) continue;
+                    DirectoryCopy(subFolderPass, newFolderPath);
+                }
+
+                if (!includePackages) continue;
+                if (subFolderName != packagesStr) continue;
+                DirectoryCopy(subFolderPass, newFolderPath);
+            }
+        }
+
+        private void DirectoryCopy(string sourcePath, string destinationPath)
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
+            DirectoryInfo destinationDirectory = new DirectoryInfo(destinationPath);
+
+            //ファイルのコピー
+            foreach (FileInfo fileInfo in sourceDirectory.GetFiles())
+            {
+                //同じファイルが存在していたら、常に上書きする
+                fileInfo.CopyTo(destinationDirectory.FullName + @"\" + fileInfo.Name, true);
+            }
+
+            //ディレクトリのコピー（再帰を使用）
+            foreach (DirectoryInfo directoryInfo in sourceDirectory.GetDirectories())
+            {
+                DirectoryCopy(directoryInfo.FullName, destinationDirectory.FullName + @"\" + directoryInfo.Name);
+            }
+        }
+    }
+}
